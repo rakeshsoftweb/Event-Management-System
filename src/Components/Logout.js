0d9c3229c5419e349d47dda2eb00451db0998a72
Rakesh Kumar <rakeshsoftweb@gmail.com>
2024-05-22 17:38:52 +0530

    Delete src directory

--- src/Components/Logout.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-const Dashboard = function () { 
-    return (
-        <div>
-            <h1>Logout</h1>
-        </div>
-    )
-}
-export default Dashboard;
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import createSagaMiddleware from "redux-saga";
-import { createStore, applyMiddleware } from 'redux';
-import { ClientReducer } from '../reducer';
-export const sagaMiddleware = createSagaMiddleware();
-export const store = createStore(ClientReducer, applyMiddleware(sagaMiddleware));
-
